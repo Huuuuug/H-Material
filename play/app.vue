92a8fb2e987b1f1e388324de0f243a939b3b72f6
cat <739068681@qq.com>
2022-12-14 10:32:42 +0800

    更改prettierrc配置

--- play/app.vue
+++ play/app.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
-    测试
-    <h-icon color="#f0f" :size="20" class="h-icon-shouqi">hello icon </h-icon>
+  <div>测试<h-icon color="#f0f" :size="20" class="h-icon-shouqi">hello icon </h-icon>
   </div>
 </template>
 <script lang="ts" setup></script>
+ 
